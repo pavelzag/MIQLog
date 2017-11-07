2678e2bbe3dfd8e2e0873de5c2f3a198f9f38b2d
Pavel <zagalsky@gmail.com>
2017-11-07 18:32:49 +0200

    Added miqlog.py

--- miqlog.py
+++ miqlog.py
@@ -5,16 +5,30 @@ import string
 from tailer import SSHTailer
 from termcolor import colored
 from time import sleep
-username = 'root'
-password = 'smartvm'
+import smtplib
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+import email.mime.application
+import configuration
+import os
+from os import path
+
+username = configuration.get_miq_config('user')
+password = configuration.get_miq_config('pass')
 random_string = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(4))
+smtp_user = configuration.get_smtp_config('user')
+smtp_passwd = configuration.get_smtp_config('pass')
 
 
-def get_log(server_parameter, password, log_level='INFO', path='NONE', log_type='None', log_write= False):
-    tailer = SSHTailer(server_parameter, path, password, verbose=True)
-    if log_write:
-        logfile = open(log_type + "__" + 'log' + "__" + random_string + '.txt', 'w')
-        print('Printing local log at: ' + str(logfile.name))
+def get_log(server_parameter, server_address, password, log_level='INFO', log_path='NONE', log_type='None', send_to = 'None'):
+    tailer = SSHTailer(server_parameter, log_path, password, verbose=True)
+    logs_directory = 'logs'
+    if not os.path.exists(logs_directory):
+        os.makedirs(logs_directory)
+    log_file_path = path.join(logs_directory, '{}_{}_{}{}'.format(log_type, 'log', random_string, '.txt'))
+    logfile = open(log_file_path, 'w')
+    print('{} {}'.format('Printing local log at:', logfile.name))
     try:
         while 1:
             for line in tailer.tail():
@@ -29,11 +43,15 @@ def get_log(server_parameter, password, log_level='INFO', path='NONE', log_type=
                     print (colored(line, 'green'))
                 elif 'ERROR' in line and 'ERROR' in log_level:
                     print (colored(line, 'red'))
-                if log_write:
-                    logfile.write("%s\n" % line)
+                logfile.write("%s\n" % line)
             sleep(0.2)
     except KeyboardInterrupt:
         tailer.disconnect()
+        logfile.close()
+        if not 'none' in send_to:
+            send_mail(send_to=send_to, subject='{}{} {}'.format(log_type, '.log incoming for', server_address), file_name=logfile.name)
+        else:
+            print('skipping e-mail')
 
 
 def log_typer(log_type_abbr):
@@ -57,20 +75,43 @@ def log_typer(log_type_abbr):
         return 'evm'
 
 
+def send_mail(send_to, subject, file_name=None, smtp_user=smtp_user, smtp_passwd=smtp_passwd,
+              server="smtp.gmail.com"):
+    msg = MIMEMultipart()
+    msg['From'] = smtp_user
+    msg['To'] = send_to
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    body = email.mime.Text.MIMEText("""Your requested log is attached here""")
+    msg.attach(body)
+
+    fp = open(file_name, 'rb')
+    att = email.mime.application.MIMEApplication(fp.read(), _subtype="txt")
+    fp.close()
+    att.add_header('Content-Disposition', 'attachment', filename=file_name)
+    msg.attach(att)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(smtp_user, smtp_passwd)
+    s.sendmail(smtp_user, send_to, msg.as_string())
+    print('{} {}'.format('sending mail to:', send_to))
+    s.quit()
+
+
 if __name__ == "__main__":
     server_address = str(raw_input('Enter your CFME address: '))
     log_level = str(raw_input('Select log level (all, info, error): ')).upper()
     log_type_abbr = str(raw_input('Select log type (evm = \'evm\', '
                              'aut = \'automation\', pol = \'policy\', '
                              'api = \'api\', prod = \'production\'): '))
-    write_to_log = str(raw_input('Do you want to write to a local log file?: (Y/N) '))
+    send_to_address = str(raw_input('Where would you like to send the mail: (Enter e-mail address or none to skip) ')).lower()
+    if not send_to_address:
+        send_to_address = 'none'
     if not log_level:
         log_level = 'ALL'
-    if write_to_log == 'Y' or len(write_to_log) > 1:
-        log_write = True
-    else:
-        log_write = False
     log_type = log_typer(log_type_abbr)
-    server_call = username + '@' + server_address
-    path = '/var/www/miq/vmdb/log/' + log_type + '.log'
-    get_log(server_call, password, log_level=log_level, path=path, log_type=log_type, log_write=log_write)
+    server_call = '{}{}{}'.format(username, '@', server_address)
+    log_path = path.join('/var/www/miq/vmdb/log/', log_type + '.log')
+    get_log(server_call, server_address, password, log_level=log_level, log_path=log_path, log_type=log_type, send_to=send_to_address)
